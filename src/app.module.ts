d411de2fc002d476ea62806626a3e79f7a489ed1
Paul Nguyen <paul@trustprovenance.com>
2025-04-08 12:02:01 +0700

    Revert "Configuring server"
    This reverts commit 009e82ef28fe91c226082c959d0b312aa46b2e06.

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-
-@Module({
-    // TODO: Add EventModule
-    imports: [],
-    controllers: [],
-    providers: [],
-})
-export class AppModule {}
--- src/config/env.config.ts
+++
@@ -1,9 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export interface EnvConfig {
-    port: number;
-}
-
-export const envConfig = registerAs('env', () => ({
-    port: parseInt(process.env.PORT || '3000', 10),
-}));
