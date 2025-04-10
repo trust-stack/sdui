53e3971dd0668d7dce6679797c4da4ed43f5b696
Paul Nguyen <paul@trustprovenance.com>
2025-04-10 07:37:16 +0700

    Adding Grid placeholder component

---
+++ src/ui/Grid.tsx
@@ -0,0 +1,18 @@
+import { View, ViewProps, withStaticProperties } from 'tamagui';
+
+type ItemProps = {
+    readonly exp?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+    readonly compact?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+    readonly medium?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+    readonly children?: React.ReactNode;
+} & ViewProps;
+
+// Placeholder component
+function Item({ ...props }: ItemProps) {
+    return <></>;
+}
+
+// TODO: Implement placeholder component
+export const Grid = withStaticProperties(View, {
+    Item,
+});
--- src/ui/form/FormContentBuilder.tsx
+++ src/ui/form/FormContentBuilder.tsx
@@ -1,7 +1,8 @@
 import { YStack } from 'tamagui';
-import { useMemo } from 'react';
+import { ReactNode, useMemo } from 'react';
 import { FieldValues } from 'react-hook-form';
 import { FormItem, FormToggle } from 'src/schema/generated';
+import { Grid } from '../Grid';
 import { FormToggleProps } from './FormToggle';
 import { Form } from './Form';
 import { useFormContext } from './context';
@@ -16,72 +17,79 @@ export function FormContentBuilder<TFormFields extends FieldValues>({
     return (
         <Form.Content>
             <YStack gap={4}>
-                {items.map((item, itemIndex) => {
-                    const key = `form-item-${itemIndex}`;
+                <Grid>
+                    {items.map((item, itemIndex) => {
+                        const key = `form-item-${itemIndex}`;
 
-                    switch (item.type) {
-                        case 'SUB_HEADER':
-                            return (
-                                <Form.Subheader key={key}>
-                                    {item.subHeader}
-                                </Form.Subheader>
-                            );
+                        const gridItem = (children: ReactNode) => (
+                            <Grid.Item
+                                key={key}
+                                exp={item.expanded}
+                                compact={item.compact}
+                            >
+                                {children}
+                            </Grid.Item>
+                        );
 
-                        // TODO: Add select components for these types
-                        case 'SELECT_LOCATION':
-                        case 'SELECT_PARTNER':
-                        case 'SELECT_SUB_LOCATION':
-                        case 'SELECT_TRADE_ITEM':
-                            return (
-                                <Form.Select<TFormFields>
-                                    key={key}
-                                    label={item.inputLabel}
-                                    id={item.validationId}
-                                    options={[]}
-                                />
-                            );
+                        switch (item.type) {
+                            case 'SUB_HEADER':
+                                return gridItem(
+                                    <Form.Subheader>
+                                        {item.subHeader}
+                                    </Form.Subheader>,
+                                );
 
-                        case 'INPUT_TEXT':
-                            return (
-                                <Form.Input<TFormFields>
-                                    key={key}
-                                    label={item.inputLabel}
-                                    id={item.validationId}
-                                />
-                            );
+                            // TODO: Add select components for these types
+                            case 'SELECT_LOCATION':
+                            case 'SELECT_PARTNER':
+                            case 'SELECT_SUB_LOCATION':
+                            case 'SELECT_TRADE_ITEM':
+                                return gridItem(
+                                    <Form.Select<TFormFields>
+                                        label={item.inputLabel}
+                                        id={item.validationId}
+                                        options={[]}
+                                    />,
+                                );
 
-                        case 'INPUT_NUMERICAL':
-                            return (
-                                <Form.NumericalInput<TFormFields>
-                                    key={key}
-                                    label={item.inputLabel}
-                                    id={item.validationId}
-                                />
-                            );
+                            case 'INPUT_TEXT':
+                                return gridItem(
+                                    <Form.Input<TFormFields>
+                                        label={item.inputLabel}
+                                        id={item.validationId}
+                                    />,
+                                );
 
-                        case 'DATE_TIME_PICKER':
-                            return (
-                                <Form.DateTimePicker<TFormFields>
-                                    key={key}
-                                    label={item.inputLabel}
-                                    id={item.validationId}
-                                />
-                            );
+                            case 'INPUT_NUMERICAL':
+                                return gridItem(
+                                    <Form.NumericalInput<TFormFields>
+                                        label={item.inputLabel}
+                                        id={item.validationId}
+                                    />,
+                                );
 
-                        case 'TOGGLE':
-                            return (
-                                <RenderFormToggle<TFormFields>
-                                    key={key}
-                                    label={item.inputLabel}
-                                    id={item.validationId}
-                                    formToggle={item.toggle}
-                                />
-                            );
+                            case 'DATE_TIME_PICKER':
+                                return gridItem(
+                                    <Form.DateTimePicker<TFormFields>
+                                        label={item.inputLabel}
+                                        id={item.validationId}
+                                    />,
+                                );
 
-                        default:
-                            return null;
-                    }
-                })}
+                            case 'TOGGLE':
+                                return gridItem(
+                                    <RenderFormToggle<TFormFields>
+                                        label={item.inputLabel}
+                                        id={item.validationId}
+                                        formToggle={item.toggle}
+                                    />,
+                                );
+
+                            default:
+                                return null;
+                        }
+                    })}
+                </Grid>
             </YStack>
         </Form.Content>
     );
