7c88c684c3c24c9a784b532a5c0994091aab75e0
Paul Nguyen <paul@trustprovenance.com>
2025-04-10 08:10:59 +0700

    Updating toggle placeholder component

--- src/ui/Toggle.tsx
+++ src/ui/Toggle.tsx
@@ -9,7 +9,22 @@ export type ToggleProps<T extends string = string> = {
 
 // TODO: Implement placeholder component
 export function Toggle<T extends string = string>({
+    options,
     ...props
 }: ToggleProps<T>) {
+    return <RadioGroup<T> items={options} {...props} />;
+}
+
+// TODO: Implement placeholder component
+type RadioGroupProps<T extends string = string> = {
+    items: {
+        label: string;
+        value: T;
+    }[];
+};
+
+function RadioGroup<T extends string = string>({
+    ...props
+}: RadioGroupProps<T>) {
     return <></>;
 }
