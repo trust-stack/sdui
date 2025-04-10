21a648d20bcf9876adb85a88b03d13a98e25179d
Paul Nguyen <paul@trustprovenance.com>
2025-04-10 10:50:26 +0700

    Adding vitest and build-validation unit test

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    test: {
+        globals: true,
+        include: ['src/**/*.spec.ts'],
+        disableConsoleIntercept: true,
+        reporters: [
+            'default',
+            [
+                'junit',
+                {
+                    outputFile: './reports/junit/unit-results.xml',
+                    suiteName: 'Unit Tests',
+                },
+            ],
+        ],
+    },
+    plugins: [swc.vite()],
+});
