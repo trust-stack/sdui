94cac4c56c8c61b17e8888e1d2ee49bfb4cfc139
Paul Nguyen <paul@trustprovenance.com>
2025-04-11 15:00:05 +0700

    Updating PagerFormBuilder, removing harvest examples

---
+++ src/ui/pager-form/PagerFormBuilder.stories.tsx
@@ -0,0 +1,27 @@
+import { View } from '@truststack/ui-kit';
+import { Meta, StoryObj } from '@storybook/react';
+import { PagerForm as PagerFormDto } from 'src/schema/generated';
+import { PlatformDecorator } from '../../storybook-utils';
+import { PagerFormBuilder } from './PagerFormBuilder';
+import data from './example.json';
+
+export default {
+    component: PagerFormBuilder,
+    decorators: [
+        (Story) => (
+            <View maxWidth={800}>
+                <Story />
+            </View>
+        ),
+    ],
+    args: {
+        formDto: data as PagerFormDto,
+        onSubmit: () => {},
+    },
+} as Meta<typeof PagerFormBuilder>;
+
+type Story = StoryObj<typeof PagerFormBuilder>;
+
+export const Web: Story = {
+    decorators: [PlatformDecorator('web')],
+};
--- src/ui/pager-form/PagerFormBuilder.tsx
+++ src/ui/pager-form/PagerFormBuilder.tsx
@@ -1,25 +1,47 @@
-import { FieldValues, Path } from 'react-hook-form';
+import { FormProvider } from '@truststack/ui-kit';
+import { Path, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { AnySchema, ObjectSchema, TypeOf } from 'yup';
 import { PagerForm as PagerFormDto } from 'src/schema/generated';
-import { FormContentBuilder } from '../form';
+import { FormContentBuilder, buildValidationSchema } from '../form';
 import { PagerForm } from './PagerForm';
-import { PagerFormProps } from './types';
 
-export type PagerFormBuilderProps<TFieldValues extends FieldValues> = {
+type FormDataType = TypeOf<ObjectSchema<Record<string, AnySchema>>>;
+
+export type PagerFormBuilderProps = {
     readonly formDto: PagerFormDto;
-} & Omit<PagerFormProps<TFieldValues>, 'forms'>;
+    readonly onSubmit: (data: FormDataType) => void;
+    readonly submitting?: boolean;
+    readonly loading?: boolean;
+};
 
-export function PagerFormBuilder<TFieldValues extends FieldValues>({
+export function PagerFormBuilder({
     formDto,
-    ...props
-}: PagerFormBuilderProps<TFieldValues>) {
+    onSubmit,
+    submitting,
+    loading,
+}: PagerFormBuilderProps) {
+    const schema = buildValidationSchema(formDto.validation);
+
+    const formMethods = useForm<TypeOf<typeof schema>>({
+        resolver: yupResolver(schema),
+        mode: 'onChange',
+        criteriaMode: 'all',
+        reValidateMode: 'onChange',
+    });
+
     return (
-        <PagerForm<TFieldValues>
-            forms={formDto.sections.map((section) => ({
-                id: section.validationId as Path<TFieldValues>,
-                title: section.title,
-                content: <FormContentBuilder items={section.items} />,
-            }))}
-            {...props}
-        />
+        <FormProvider formMethods={formMethods}>
+            <PagerForm<TypeOf<typeof schema>>
+                forms={formDto.sections.map((section) => ({
+                    id: section.validationId as Path<TypeOf<typeof schema>>,
+                    title: section.title,
+                    content: <FormContentBuilder items={section.items} />,
+                }))}
+                onSubmit={() => onSubmit(formMethods.getValues())}
+                submitting={submitting}
+                loading={loading}
+            />
+        </FormProvider>
     );
 }
