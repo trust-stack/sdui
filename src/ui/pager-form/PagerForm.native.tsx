e1b371fb5b115f073087a97174599afe14418020
Paul Nguyen <paul@trustprovenance.com>
2025-04-11 13:59:39 +0700

    Adding TODO

--- src/ui/pager-form/PagerForm.native.tsx
+++ src/ui/pager-form/PagerForm.native.tsx
@@ -3,6 +3,7 @@ import { PagerFormProps } from './types';
 
 // TODO: Move to @trust-stack/ui-kit?
 
+// TODO: Implement
 export function PagerForm<TFieldValues extends FieldValues>({
     ...props
 }: PagerFormProps<TFieldValues>) {
