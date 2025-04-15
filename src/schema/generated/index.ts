c91d11c0ba80e8d6703d325768eac1a70c569c7d
Paul Nguyen <paul@trustprovenance.com>
2025-04-15 07:30:16 +0700

    Updating 'extended', 'compact' type

--- src/schema/generated/index.ts
+++ src/schema/generated/index.ts
@@ -21,8 +21,8 @@ export interface FormItem {
   inputLabel?: string;
   select?: FormSelect;
   toggle?: FormToggle;
-  expanded?: number;
-  compact?: number;
+  expanded?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  compact?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
   [k: string]: unknown;
 }
 export interface FormSelect {
